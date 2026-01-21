ba42412e3c06c9b34feecda6c5d879ac90bac203
Elsa Lopez Perez <elsa.lopez.133@outlook.es>
2026-01-21 10:58:08 +0100

    Adding .gitignore

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,4 +1,5 @@
 docker build -t formal-workshop Docker/Dockerfile .
 
-# docker run --rm formal-workshop tamarin-prover --sapic 04_auth_and_secrecy_hold.spthy --prove
+# docker build -t formal-workshop -f Docker/Dockerfile .
+# docker run --rm formal-workshop tamarin-prover 04_auth_and_secrecy_hold.spthy --prove
 # docker run -it --rm formal-workshop
