e10b413713bfc12c1a3572df33f660ebefe1a4a4
Elsa Lopez Perez <elsa.lopez.133@outlook.es>
2026-01-29 11:23:47 +0100

    docker file update

--- Docker/build.sh
+++ Docker/build.sh
@@ -1,5 +1,4 @@
-docker build -t formal-workshop Docker/Dockerfile .
+docker build -t formal-workshop -f Docker/Dockerfile .
 
-# docker build -t formal-workshop -f Docker/Dockerfile .
 # docker run --rm formal-workshop tamarin-prover 04_auth_and_secrecy_hold.spthy --prove
 # docker run -it --rm formal-workshop
